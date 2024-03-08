15f7ca71e9af69b060eb38376de3ad72758f9a6f
Marco La Franca <marcantonio.lafranca@eng.it>
2024-03-08 19:03:17 +0100

    Initial commit

---
+++ docker/local-api/data/scripts/cert.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+if ! keytool -import -trustcacerts -keystore /opt/java/openjdk/lib/security/cacerts -storepass changeit -noprompt -alias dataAppCert -file /cert/execution_core_container.cer; then
+    echo "---"
+fi
